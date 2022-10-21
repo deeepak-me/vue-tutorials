51ef46eb87ae9cf7e540c05699bf054cdb690e63
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-10-21 16:17:39 +0530

    feat: about routes

--- flo-jobs/src/router/index.js
+++ flo-jobs/src/router/index.js
@@ -1,15 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     component: About
   }
 ]
