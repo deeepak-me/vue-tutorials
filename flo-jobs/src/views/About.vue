7400e07f8b8c3d26067deb496e5c8da99444de08
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-10-21 16:18:07 +0530

    feat: adding contents in about page

---
+++ flo-jobs/src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem
+      ab explicabo alias facilis nemo libero est vero praesentium non laboriosam
+      cupiditate et, ex dignissimos, repudiandae unde voluptatum aspernatur
+      atque iste natus culpa, debitis dicta tenetur. Voluptates possimus
+      repellat nemo adipisci mollitia deserunt odit natus ab eaque. Voluptatem
+      quaerat nemo accusantium accusamus aliquid explicabo possimus dolores
+      soluta sit, animi iure assumenda qui expedita laboriosam minus impedit
+      minima dolorum iste! Similique, accusamus facilis iste pariatur hic
+      tempore iure tenetur distinctio architecto temporibus aut unde facere
+      molestiae praesentium non eum reprehenderit consectetur! Necessitatibus,
+      pariatur. Totam ullam et, deserunt nam sint laudantium id voluptatum?
+    </div>
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni ipsa ad
+      adipisci praesentium deserunt in cupiditate aut esse corrupti quam velit
+      ab optio obcaecati ipsam commodi, qui laudantium, nemo laboriosam magnam
+      sunt sapiente cum dolore mollitia quod? Cupiditate labore cumque, amet
+      quia, dolore, possimus porro sunt esse ipsa fugit explicabo.
+    </div>
+  </div>
+</template>
