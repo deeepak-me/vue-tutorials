c5a52135fe8a9f9e49a02dfab7668f1cff6ed419
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-10-21 16:18:39 +0530

    feat:adding contents and removig unwanted

---
+++ flo-jobs/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>HomePage</h1>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit,
+      vero magni doloremque, dolores natus provident laudantium voluptate eum
+      non, dolor accusantium ea quisquam iure enim. Praesentium architecto
+      incidunt porro tempore repellat quidem libero cupiditate minus, neque quas
+      commodi animi. Natus eligendi ea ut sit modi aliquid, iste optio sed
+      pariatur voluptates exercitationem commodi dolorum. Sunt dolor fugit
+      aperiam nostrum et ipsam, ad officiis recusandae architecto culpa itaque
+      beatae! Eos, atque error. Facere quas blanditiis error, voluptates
+      voluptatem architecto reprehenderit. Ex vel sed maxime vero ducimus
+      assumenda quidem beatae, quam, incidunt architecto voluptatem culpa non
+      ipsum. Molestiae facere maiores dolores iste!
+    </div>
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam cumque
+      fugiat eveniet debitis, asperiores et reiciendis quidem ea quo, molestias
+      aut dignissimos quibusdam ad nam. Dignissimos accusantium rerum labore
+      fuga reiciendis. Nihil tempora, aperiam consequatur fugit officia libero
+      nam vero totam eaque pariatur magni unde animi velit non. Omnis, culpa?
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
