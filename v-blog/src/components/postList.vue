9aeab4867ada0f6aa5baa93eca7274e9dee9853a
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-10-28 16:03:53 +0530

    feat: import single post

---
+++ v-blog/src/components/postList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="postList">
+    <div v-for="post in posts" :key="post.id">
+      <singlePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import singlePost from "./singlePost.vue";
+export default {
+  props: ["posts"],
+  components: { singlePost },
+  setup(props) {},
+};
+</script>
+
+<style></style>
