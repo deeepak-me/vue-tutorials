068657f87f7ad27045d096c61396632803a88358
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-10-28 16:02:00 +0530

    feat: new pages added

--- v-blog/src/views/HomeView.vue
+++ v-blog/src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>HOME</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <postList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
+import postList from "../components/postList.vue";
+import getPosts from "../composables/getPosts";
 export default {
   name: "HomeView",
+  components: { postList },
+  setup() {
+    const { posts, error, load } = getPosts();
+
+    load();
+
+    return { posts, error };
+  },
 };
 </script>
